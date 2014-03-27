64ed2f18c5dbb83e8de170b76a1cec7d4ed46c55
macmorning <sylvain.yvon@gmail.com>
2014-03-27 07:19:39 +0100

    todo : compare size of messages array with the current counter when adding a message

--- src/js/chessHubBoard-0.1.0.js
+++ src/js/chessHubBoard-0.1.0.js
@@ -51,6 +51,7 @@ var CHESSBOARD = {
     timerInterval: '',              // set to an interval when a countdown is required
     currentGameTurn: '',            // 'w' or 'b'
     gameHistory: [],                // turns history
+    counter: 0,
     chessBoard: { 11:'', 12:'', 13:'', 14:'', 15:'', 16:'', 17:'', 18:'', 
                     21:'', 22:'', 23:'', 24:'', 25:'', 26:'', 27:'', 28:'', 
                     31:'', 32:'', 33:'', 34:'', 35:'', 36:'', 37:'', 38:'', 
@@ -913,6 +914,7 @@ var CHESSBOARD = {
         CHESSBOARD.whiteCanCastleQueenSide = true;
         CHESSBOARD.currentGameTurn= '';
         CHESSBOARD.gameHistory = [];
+        CHESSBOARD.counter = 0;
         return 0;
     },
     
